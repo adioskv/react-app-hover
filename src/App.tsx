776649de91bad9b3eb8593b657eb15f1007a0299
Serhii Mykhailivskyi <sergeymih098123@gmail.com>
2024-01-22 15:10:29 +0200

    rewrite select logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,30 @@ import { Field } from './components/Field/Field';
 import { Select } from './components/Select/Select';
 import { History } from './components/History/History';
 import './App.scss';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { FieldContext } from './context/FieldContext';
+import { Option } from './types/Option';
+import axios from 'axios';
 
 export const App = () => {
-
   const { isStarted } = useContext(FieldContext);
+  const [options, setOptions] = useState<Option[]>([]);
+  const [error, setError] = useState('');
+
+  const loadData = async () => {
+    try {
+      const data = (await axios.get("https://60816d9073292b0017cdd833.mockapi.io/modes")).data;
+
+      setOptions(data);
+    } catch (e) {
+      setError('Unable to load data')
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
   return (
     <>
@@ -15,7 +33,10 @@ export const App = () => {
 
       <div className="app__container">
         <div className='field__container'>
-          <Select/>
+          {error 
+            ? <p className='select--error'>{error}</p>
+            : <Select options={options}/>
+            }
           <Field/>
         </div>
 
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,31 +1,14 @@
-import axios from 'axios';
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useState } from 'react'
 import { Option } from '../../types/Option';
 import './Select.scss'
 import classNames from 'classnames';
 import { FieldContext } from '../../context/FieldContext';
 import { ActiveContext } from '../../context/ActiveContext';
 
-export const Select = () => {
-  const [options, setOptions] = useState<Option[]>([]);
+export const Select = ({ options } : {options: Option[]}) => {
   const { fields, setFields, isStarted, setIsStarted } = useContext(FieldContext);
   const { setActiveCells } = useContext(ActiveContext);
   const [error, setError] = useState('');
-
-  const loadData = async () => {
-    try {
-      const data = (await axios.get("https://60816d9073292b0017cdd833.mockapi.io/modes")).data;
-
-      setOptions(data);
-    } catch (e) {
-      setError('Unable to load data')
-      console.error(e);
-    }
-  }
-
-  useEffect(() => {
-    loadData();
-  }, []);
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setError('')
